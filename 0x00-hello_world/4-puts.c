00e39de117bed0418c43147531a63fadb9bdf70d
Kennedy-Amos <kennedyamosedesiri@gmail.com>
2022-09-08 08:45:15 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <studio.h>
 
 /**
- * main - print the string in the out function
- *
- * Description: using the main function
- * this program prints 'programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-  puts("\"programming is like bulding a multilingual puzzle");
-  return (0);
+       puts("\"programming is like building a multilingual puzzle");
+       return (0);
 }

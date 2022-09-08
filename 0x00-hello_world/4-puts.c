581c0fa86a57bf7c299e4ee32a92c6c01272493b
Kennedy-Amos <kennedyamosedesiri@gmail.com>
2022-09-08 08:35:55 -0700

    put programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main - print the string in the out function
+ *
+ * Description: using the main function
+ * this program prints 'programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+  puts("\"programming is like bulding a multilingual puzzle");
+  return (0);
+}
